85c5f236234d99d60d03361f3803726c91d62d39
Levina Tani <edelaida@gmail.com>
2024-10-09 11:35:26 +0300

    new todo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//import { Counter } from "./components/Counter/Counter";
+import { TodoList } from "./components/TodoList/TodoList";
+//import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <TodoList />
+      {/* <Counter /> */}
+    </>
+  );
+};
+
+export default App;
